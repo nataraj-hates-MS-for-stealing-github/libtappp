43c16ed216c7a3ff62d26d196b2b4e7ae7dfba7d
Paulo Custodio <pauloscustodio@gmail.com>
2016-05-02 22:27:34 +0100

    clang++ complains 'exception::what' hides overloaded virtual function
    Change what() signature to the same as the base function
    virtual const char* what() const noexcept

--- t/00-sanity.cpp
+++ t/00-sanity.cpp
@@ -11,7 +11,7 @@ class exception : public std::exception {
 public:
     exception(const std::string& _message) : message(_message) {
     }
-    const char* what() {
+    virtual const char* what() const noexcept {
         return message.c_str();
     }
     ~exception() throw() {
